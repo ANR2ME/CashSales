6b9c5d8a8cc2c2706db2ce29c2b49533565bc76a
AdamN <anr2k@mail.com>
2014-09-02 10:17:06 +0700

    Changing Database name to CashSales

--- Data/Context/OffsetPrintingSuppliesEntities.cs
+++ Data/Context/OffsetPrintingSuppliesEntities.cs
@@ -11,7 +11,7 @@ namespace Data.Context
 {
     public class OffsetPrintingSuppliesEntities : DbContext
     {
-        public OffsetPrintingSuppliesEntities()
+        public OffsetPrintingSuppliesEntities() : base("CashSales")
         {
             Database.SetInitializer<OffsetPrintingSuppliesEntities>(new DropCreateDatabaseIfModelChanges<OffsetPrintingSuppliesEntities>());
         }

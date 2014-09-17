0867175d1f484f5123589befb5652557978c2379
William Lukman <williamlukman@gmail.com>
2014-09-17 17:33:47 +0700

    GL Posting

--- Service/Service/Accounting/ClosingService.cs
+++ Service/Service/Accounting/ClosingService.cs
@@ -173,7 +173,8 @@ namespace Service.Service
 
         public bool IsDateClosed(DateTime DateToCheck)
         {
-            var ClosedDates = _repository.FindAll(x => x.BeginningPeriod >= DateToCheck && x.EndDatePeriod < DateToCheck.AddDays(1));
+            DateTime dateEnd = DateToCheck.AddDays(1);
+            var ClosedDates = _repository.FindAll(x => x.BeginningPeriod <= DateToCheck && x.EndDatePeriod > dateEnd).ToList();
             if (ClosedDates.Any())
             {
                 return true;
